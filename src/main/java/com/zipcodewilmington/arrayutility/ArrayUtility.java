401406616d3d2ab41f184d24271f7ecf31459d82
PGowri <gowri.palaniswamy@gmail.com>
2024-01-06 18:09:49 -0500

    all tests passing

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,6 +1,8 @@
 package com.zipcodewilmington.arrayutility;
 
+
 import java.lang.reflect.Array;
+
 
 /**
  * Created by leon on 3/6/18.
@@ -13,13 +15,43 @@ public class ArrayUtility <T> {
         array = inputArray;
     }
 
-//    public Integer CountDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
-//
-//    }
-//
-//    public T getMostCommonFromMerge(T[] arrayToMerge) {
-//
-//    }
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
+        int count = 0;
+        T[] mergedArray = concatArrays( array,arrayToMerge);
+        for(int i = 0; i<mergedArray.length; i ++) {
+            if(mergedArray[i] == valueToEvaluate){
+                count ++;
+            }
+        }
+        return count;
+    }
+    public static <T> T[] concatArrays (T[]array1, T[]array2) {
+        int array1length = array1.length;
+        int array2length = array2.length;
+        int totalLen = array1length + array2length;
+
+        T[] all = getArray(totalLen);
+        int copied = 0;
+            System.arraycopy(array1, 0, all, copied, array1.length);
+            System.arraycopy(array2, copied, all, array1length, array2.length);
+
+        return all;
+    }
+    public static <T> T[] getArray(int size) {
+        T[] genericArray = (T[]) new Object[size];
+        return genericArray;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        T[] mergedArray = concatArrays( array,arrayToMerge);
+        T res = null;
+        for(int i = 0; i< array.length; i ++){
+            if (mergedArray[i] == array[i]){
+                res =  mergedArray[i];
+            }
+        }
+        return res;
+    }
 
     public Integer getNumberOfOccurrences(T valueToEvaluate){
         int count = 0;
